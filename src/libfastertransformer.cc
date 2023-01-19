2086fc63ffa521f05f1610daf54cc05dc4e7354b
Christopher Pirillo <pirillo@google.com>
2023-01-19 15:02:14 -0800

    Add additional logging to bad multi-gpu scenarios

--- src/libfastertransformer.cc
+++ src/libfastertransformer.cc
@@ -370,10 +370,12 @@ ModelState::ModelState(TRITONBACKEND_Model* triton_model)
     instance_group_count == 1) || (!multi_node_enabled && gpu_size % tp_pp_size == 0 &&
     model_instance_size * instance_group_count >=  gpu_size);
   if (!multi_model_instance_valid) {
+    std::stringstream formatted_error;
+    ss << "1. Number of visible GPUs (" << gpu_size << ") must be evenly divisble by TP * PP (" << tp_pp_size << ") \n"
+      "2. Number of visible GPUs (" << gpu_size << ") must be <= instance count * TP * PP (" << instance_group_count * tp_pp_size << ") \n"
+      "3. Multi-Node Inference only support one model instance (nodes requested: (" << num_nodes << "), instances requested: (" << instance_group_count << ")) \n"
     THROW_IF_BACKEND_MODEL_ERROR(TRITONSERVER_ErrorNew(TRITONSERVER_ERROR_UNSUPPORTED,
-      "1. Number of visible GPUs must be evenly divisble by TP * PP \n"
-      "2. Number of visible GPUs must be <= instance count * TP * PP \n"
-      "3. Multi-Node Inference only support one model instance \n"));
+      ));
   }
 
   int64_t max_batch_size = 0;
